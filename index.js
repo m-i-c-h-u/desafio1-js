7f4807fef08f6edc86b88b0029156a3b8e1b0c7a
Michelle Valdés <michelle.vlds1997@gmail.com>
2024-10-18 20:40:23 -0300

    img add

--- index.js
+++ index.js
@@ -2,3 +2,35 @@ precio = 400000
 
 precioSpan = document.querySelector(".precio-inicial");
 precioSpan.innerHTML = precio
+
+cantidad = 0
+
+cantidadSpan = document.querySelector(".cantidad");
+cantidadSpan.innerHTML = cantidad
+
+function sumar() {
+    cantidad = cantidad + 1
+    document.querySelector(".cantidad").innerHTML = cantidad
+}
+
+function restar() {
+    if (Number(cantidad) == 0) {
+        return
+    }
+    cantidad = cantidad - 1
+    document.querySelector(".cantidad").innerHTML = cantidad
+}
+
+function calcular() {
+    if (Number(cantidad) == 0) {
+        document.querySelector(".valor-total").innerHTML = 0
+        return
+    }
+    total = Number(total) - Number(precio)
+    document.querySelector(".valor-total").innerHTML = total
+}
+
+function multiplicar() {
+    total = Number(precio) * Number(cantidad)
+    document.querySelector(".valor-total").innerHTML = total
+}
